fd3fb858197ce89b9addeb31ae352cbc7b59a72b
Juan Afanador <juanafanador07@gmail.com>
2025-01-10 15:48:13 -0500

    Create Hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+  env: {
+    schema: {
+      AVAILABLE_TO_WORK: envField.boolean({
+        access: "public",
+        context: "client",
+        default: true,
+        optional: false,
+      }),
+    },
+  },
+});
