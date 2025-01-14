1833bc0cae1f4f881b654b4e2f274d772322868a
Juan Afanador <juanafanador07@gmail.com>
2025-01-14 16:45:06 -0500

    Remove ASTRO_SITE env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,27 @@
 // @ts-check
-import { loadEnv } from "vite";
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import favicons from "astro-favicons";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-const { ASTRO_SITE } = loadEnv(process.env.ASTRO_SITE || "", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: ASTRO_SITE,
-  integrations: [tailwind(), icon(), favicons({
-    name: "Portafolio de Juan Afanador",
-    short_name: "Portafolio",
-    themes: ["#ecfdf5", "#022c22"],
-  }), sitemap(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    icon(),
+    favicons({
+      name: "Portafolio de Juan Afanador",
+      short_name: "Portafolio",
+      themes: ["#ecfdf5", "#022c22"],
+    }),
+    sitemap(),
+    robotsTxt(),
+  ],
   env: {
     schema: {
       AVAILABLE_TO_WORK: envField.boolean({
-        access: "public",
-        context: "client",
-        default: true,
-        optional: false,
-      }),
-      ASTRO_SITE: envField.string({
         access: "public",
         context: "client",
         optional: false,
